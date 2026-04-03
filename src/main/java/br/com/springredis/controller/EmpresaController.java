5dfd0a29350ec140f39c3bdd216b7740c1971320
andre <andresousalee@gmail.com>
2026-04-03 12:54:15 -0300

    criação de controle e entidade empresa

---
+++ src/main/java/br/com/springredis/controller/EmpresaController.java
@@ -0,0 +1,25 @@
+package br.com.springredis.controller;
+
+
+import br.com.springredis.entity.Empresa;
+import br.com.springredis.service.EmpresaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/empresa")
+public class EmpresaController {
+
+    @Autowired
+    private EmpresaService empresaService;
+
+    @GetMapping
+    public ResponseEntity<List<Empresa>> findAll() {
+        return ResponseEntity.ok(empresaService.findAllComCache());
+    }
+}
